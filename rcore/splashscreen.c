517dc4fe0d36424778652387137fab9a2d5828a2
Stubenhocker <Stubenhocker1399@gmail.com>
2017-08-06 16:09:18 +1000

    Add splashscreen stub after display init

---
+++ rcore/splashscreen.c
@@ -0,0 +1,16 @@
+/* display.c
+ *
+ * RebbleOS
+ *
+ * Author: Barry Carter <barry.carter@gmail.com>
+ */
+
+
+#include "rebbleos.h"
+
+void display_splash_screen(void){
+    //TODO Actully load a splash screen
+    ;
+
+    KERN_LOG("Display", APP_LOG_LEVEL_INFO, "Splash screen loaded");
+}
---
+++ rcore/splashscreen.h
@@ -0,0 +1,11 @@
+#pragma once
+/* splashscren.h
+ * 
+ * RebbleOS
+ *
+ * Author: Stubenhocker
+ */
+
+#include "FreeRTOS.h"
+
+void display_splash_screen(void);
