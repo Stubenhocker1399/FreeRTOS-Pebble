acaa742056e54f4b6289e56bc8ca6502fa15d36d
Stubenhocker <Stubenhocker1399@gmail.com>
2017-08-13 17:02:01 +1000

    Revert "Draw random noise to framebuffer"
    This reverts commit fa9c94ba7bec22002814bd0c26e3ad09e1d3d88d.

--- rcore/splashscreen.c
+++ rcore/splashscreen.c
@@ -6,29 +6,11 @@
  */
 
 
-//#include "rebbleos.h"
-#include "display.h"
-//#include "png.h"
-//#include "resource.h"
-#include "common.h"
+#include "rebbleos.h"
 
 void display_splash_screen(void){
-    //TODO Actually load a splash screen
-    uint8_t* framebuffer = display_get_buffer();
-    //void resource_load_id_system(uint16_t resource_id, uint8_t *buffer)
-    //resource_load_id_system(25,framebuffer);
-    int x,y;
-    for (x=0; x<144; x++)
-    {
-        for (y=0;y<168;y++){
-            if(rand()%2==0){
-                framebuffer[y * __SCREEN_FRAMEBUFFER_ROW_BYTE_AMOUNT + x] = 0xFF;
-            }
-        }
-    }
-    display_draw();
-    //display_reset(false);
-    //display_init();
-    //for(;;);
+    //TODO Actully load a splash screen
+    ;
+
     KERN_LOG("Display", APP_LOG_LEVEL_INFO, "Splash screen loaded");
 }
